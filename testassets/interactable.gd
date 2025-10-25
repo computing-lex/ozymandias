9691ea4f8e3adb3a455927cd136f2a36d91df07f
Squirrel-Enthusiast <Alex.R.Brown.06@gmail.com>
2025-10-25 13:24:57 -0400

    Game
    I made game

---
+++ testassets/interactable.gd
@@ -0,0 +1,16 @@
+extends Node2D
+
+@export var Text1 = ""
+
+@export var Use_States = false
+
+@export var State = 0
+@export var Max_State = 0
+
+@export var Text2 = ""
+@export var Text3 = ""
+@export var Text4 = ""
+@export var Text5 = ""
+
+@onready var Visual = $Visual
+@onready var Area = $Area
