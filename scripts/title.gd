738fee13bd33db4d81767b6d3a4556147479804a
Squirrel-Enthusiast <Alex.R.Brown.06@gmail.com>
2025-10-26 15:38:49 -0400

    Title
    Title screen added

--- scripts/title.gd
+++ scripts/title.gd
@@ -1,9 +1,25 @@
 extends Node2D
 
 var next_scene = "res://assets/levels/prelevel1.tscn"
+@onready var Audio = $AudioStreamPlayer
+@onready var Anim = $AnimationPlayer
 
 func _ready() -> void:
-	start_game()
+	Audio.play()
 
 func start_game():
 	get_tree().change_scene_to_file(next_scene)
+
+
+func _on_audio_stream_player_finished() -> void:
+	Audio.play()
+
+
+func _on_dev_button_pressed() -> void:
+	if Anim.is_playing() == false:
+		Anim.play("startgame")
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "startgame":
+		start_game()
