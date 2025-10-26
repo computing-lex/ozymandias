bb6f19fc880708a3da5367276dc506515b9286a3
Squirrel-Enthusiast <Alex.R.Brown.06@gmail.com>
2025-10-26 00:38:20 -0400

    Changes
    I made some changes

--- scripts/fake_intro.gd
+++ scripts/fake_intro.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
+@onready var Bar = $ProgressBar
+@onready var Percent = $ProgressText
+@onready var Anim = $AnimationPlayer
+
 var next_scene = "res://assets/levels/title.tscn"
 
 func _ready() -> void:
-	intro_complete()
+	Anim.play("Loading")
+
+func _physics_process(delta: float) -> void:
+	Percent.text = str(floor(Bar.visible_ratio*100))+"%"
 
 func intro_complete():
 	get_tree().change_scene_to_file(next_scene)
+
+
+func _on_animation_player_animation_finished(anim_name: StringName) -> void:
+	if anim_name == "Loading":
+		intro_complete()
